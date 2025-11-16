356b27dbd5db0b71629c2d678762620b98eb5299
Jose Adrian Luza Carhuamaca <u202213404@upc.edu.pe>
2025-11-15 23:26:09 -0500

    Embed YouTube video in Contact component
    Added an embedded YouTube video to the Contact component.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -174,6 +174,20 @@ export const Contact = () => {
                 </div>
               </CardContent>
             </Card>
+
+            {/* Embedded Video */}
+            <div className="flex justify-center mt-8">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/bvLZ6uHdSzw?si=9mfefOHgvyCKDNW5"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            </div>
           </div>
         </div>
       </div>
